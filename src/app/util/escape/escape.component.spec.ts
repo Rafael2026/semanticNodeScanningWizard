ef1117e241c74c6d83a1c6829f292e4c0f5ec867
Rafael Aguilar Muñoz <rafaaguilarmunoz@gmail.com>
2022-05-23 14:03:15 +0200

    Add files via upload

---
+++ src/app/util/escape/escape.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EscapeComponent } from './escape.component';
+
+describe('EscapeComponent', () => {
+  let component: EscapeComponent;
+  let fixture: ComponentFixture<EscapeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EscapeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EscapeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
