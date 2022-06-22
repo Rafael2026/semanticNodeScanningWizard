696ccdbcde463b1e21d11027a1f0e4b91d4e981d
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-06-22 11:56:46 +0200

    Replay project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataGraphService } from './services/data-graph.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+
+export class AppComponent implements OnInit {
+  initGraph: boolean = false;
+
+  constructor(public dataGraphService: DataGraphService) { }
+
+  ngOnInit(): void { }
+
+  onReady() {
+    this.initGraph = true;
+  }
 }
